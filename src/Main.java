88236f9271ad93b2d41a66e85d60a3e0ac144e7a
Alireza Farahani <ar.d.farahani@gmail.com>
2019-05-31 22:23:07 +0430

    fixing Byte-order-mark and path slash

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		Poem myPoem = new Poem();
-		myPoem.init(".\\in\\poem.txt");
+		myPoem.init("./in/poem1.txt");
 		ArrayList<Distich> distichList = myPoem.generateDistichList();
 		myPoem.printFirstInformation();
 		myPoem.printDistichList(distichList);
--- src/poem/generator/data/Poem.java
+++ src/poem/generator/data/Poem.java
@@ -3,94 +3,93 @@ package poem.generator.data;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
 /**
  * @author Elyas-Dolatabadi
- * 
  */
 public class Poem {
 
-	private String mainMeter;
-	private List<Word> words;
+    private String mainMeter;
+    private List<Word> words;
 
-	public Poem() {
-		this.words = new ArrayList<Word>();
-	}
+    public Poem() {
+        this.words = new ArrayList<Word>();
+    }
 
-	public void init(String fileName) {
-		ReadFileIntoList reader = new ReadFileIntoList();
+    public void init(String fileName) {
+        ReadFileIntoList reader = new ReadFileIntoList();
 
-		List<String> linesList = reader.ReadFile(fileName);
-		Iterator<String> itr = linesList.iterator();
-		String firstLine = itr.next();
-		this.mainMeter = firstLine.substring(1, firstLine.length());
+        List<String> linesList = reader.ReadFile(fileName);
+        Iterator<String> itr = linesList.iterator();
+        this.mainMeter = itr.next();
 
-		while (itr.hasNext()) {
-			String hemistich = itr.next();
-			if (hemistich != null && !hemistich.isEmpty()) {
-				Word word = wordDelimiter(hemistich);
-				words.add(word);
-			}
-		}
-	}
+        while (itr.hasNext()) {
+            String hemistich = itr.next();
+            if (hemistich != null && !hemistich.isEmpty()) {
+                Word word = wordDelimiter(hemistich);
+                words.add(word);
+            }
+        }
+    }
 
-	public void printFirstInformation() {
-		System.out.println("Main Meter:");
-		System.out.println("("+this.mainMeter+")");
-		System.out.println("Words:");
-		
-		for (int i = 0; i < words.size(); i++) {
-			System.out.println("("+ words.get(i).getTitle() + ")-(" + words.get(i).getMeter()+ ")");
-		}
-	}
+    public void printFirstInformation() {
+        System.out.println("Main Meter:");
+        System.out.println("(" + this.mainMeter + ")");
+        System.out.println("Words:");
 
-	public ArrayList<Distich> generateDistichList() {
-		ArrayList<Distich> res = new ArrayList<Distich>();
+        for (int i = 0; i < words.size(); i++) {
+            System.out.println("(" + words.get(i).getTitle() + ")-(" + words.get(i).getMeter() + ")");
+        }
+    }
 
-		int numberOfMainMeterWords = this.mainMeter.split(" ").length;
+    public ArrayList<Distich> generateDistichList() {
+        ArrayList<Distich> res = new ArrayList<Distich>();
 
-		for (int i = 0; i < numberOfMainMeterWords; i++) {
-			Distich distich = new Distich(this.mainMeter);
-			for (int j = 0; j < numberOfMainMeterWords; j++) {
-				for (Word word : this.words) {
-					distich.addWord(word);
-					if (distich.isMatchWithMainMeter()) {
-						if ( distich.getMeter().length()== mainMeter.length()&& !listHasThisDistich(res, distich.getTitle())) {
-							res.add(distich);
-						}
-					} else {
-						distich.clearLastItem();
-					}
-				}
-			}
-		}
+        int numberOfMainMeterWords = this.mainMeter.split(" ").length;
 
-		return res;
-	}
+        for (int i = 0; i < numberOfMainMeterWords; i++) {
+            Distich distich = new Distich(this.mainMeter);
+            for (int j = 0; j < numberOfMainMeterWords; j++) {
+                for (Word word : this.words) {
+                    distich.addWord(word);
+                    if (distich.isMatchWithMainMeter()) {
+                        if (distich.getMeter().length() == mainMeter.length() && !listHasThisDistich(res, distich.getTitle())) {
+                            res.add(distich);
+                        }
+                    } else {
+                        distich.clearLastItem();
+                    }
+                }
+            }
+        }
 
-	public void printDistichList(ArrayList<Distich> list) {
-		System.out.println("Generated Poem:");
-		for (Distich distich : list) {
-			System.out.println(distich.getTitle());
-		}
-	}
+        return res;
+    }
 
-	private Word wordDelimiter(String hemistich) {
+    public void printDistichList(ArrayList<Distich> list) {
+        System.out.println("Generated Poem:");
+        for (Distich distich : list) {
+            System.out.println(distich.getTitle());
+        }
+    }
 
-		String title = hemistich.substring(0, hemistich.indexOf(" "));
-		String meter = hemistich.substring(hemistich.indexOf(" ") + 1,
-				hemistich.length());
+    private Word wordDelimiter(String hemistich) {
 
-		return new Word(title, meter);
-	}
+        String title = hemistich.substring(0, hemistich.indexOf(" "));
+        String meter = hemistich.substring(hemistich.indexOf(" ") + 1,
+                hemistich.length());
 
-	private Boolean listHasThisDistich(final List<Distich> list,final String distichTitle) {
-		for (Distich item : list) {
-			String itemTitle = item.getTitle();
-			if (distichTitle.equals(itemTitle)) {
-				return true;
-			}
-		}
-		return false;
-	}
+        return new Word(title, meter);
+    }
+
+    private Boolean listHasThisDistich(final List<Distich> list, final String distichTitle) {
+        for (Distich item : list) {
+            String itemTitle = item.getTitle();
+            if (distichTitle.equals(itemTitle)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
